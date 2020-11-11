3dbdd6f614666ef6d00d5ede9c07bfde57346114
Norfarisha Jefree <72587541+siseisya@users.noreply.github.com>
2020-11-11 20:01:38 +0800

    Initial commit

---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+
+<h1>Lab Test 2</h1>
+
+  <?php 
+  
+  echo "Welcome, this is your email : ";
+  echo  $_POST["femail"];
+  
+  
+  
+  ?>
+</body>
+</html>
